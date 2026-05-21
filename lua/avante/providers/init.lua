26b8ae60912ed69c05a23df39dfc70813c82252d
飞羏 <feixiang.yfx@alibaba-inc.com>
2026-05-21 14:08:29 +0800

    refactor: rm cmt

--- lua/avante/providers/init.lua
+++ lua/avante/providers/init.lua
@@ -204,7 +204,6 @@ M = setmetatable(M, {
 function M.setup()
   vim.g.avante_login = false
 
-  -- Config.provider = "aone"
   if Config.acp_providers[Config.provider] then return end
 
   ---@type AvanteProviderFunctor | AvanteBedrockProviderFunctor
