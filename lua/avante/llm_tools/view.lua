d359645015cd7c1f551d3c447691872344cbd9f8
飞羏 <feixiang.yfx@alibaba-inc.com>
2026-06-04 10:26:00 +0800

    feat: 放开view的权限

--- lua/avante/llm_tools/view.lua
+++ lua/avante/llm_tools/view.lua
@@ -93,7 +93,7 @@ function M.func(input, opts)
   if not input.path then return false, "path is required" end
   if on_log then on_log("path: " .. input.path) end
   local abs_path = Helpers.get_abs_path(input.path)
-  if not Helpers.has_permission_to_access(abs_path) then return false, "No permission to access path: " .. abs_path end
+  -- if not Helpers.has_permission_to_access(abs_path) then return false, "No permission to access path: " .. abs_path end
   if not Path:new(abs_path):exists() then return false, "Path not found: " .. abs_path end
   if Path:new(abs_path):is_dir() then return false, "Path is a directory: " .. abs_path end
   local file = io.open(abs_path, "r")
